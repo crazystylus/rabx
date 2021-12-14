172466089d9b2824bcabfb86a047e7c53def12b0
Kartik Sharma <kartik.sharma522@gmail.com>
2021-12-14 23:55:59 +0530

    Added persistence by storing data in log format Currently db file name is hard-coded to db.bson Now cli is functional and will read complete db on each get

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,12 +1,27 @@
+use anyhow::Result;
 use clap::{load_yaml, App};
 
-fn main() {
+fn main() -> Result<()> {
     let yaml = load_yaml!("../../cli.yaml");
     let matches = App::from(yaml).get_matches();
+    let mut db_store = kvs::KvStore::new();
     match matches.subcommand() {
-        Some(("set", _)) => panic!("unimplemented"),
-        Some(("get", _)) => panic!("unimplemented"),
-        Some(("rm", _)) => panic!("unimplemented"),
+        Some(("set", sub_matches)) => {
+            let key = sub_matches.value_of("KEY").expect("Required");
+            let value = sub_matches.value_of("VALUE").expect("Required");
+            db_store.set(key.to_string(), value.to_string())?;
+            println!("SET: {} -> {}", key, value);
+        }
+        Some(("get", sub_matches)) => {
+            let key = sub_matches.value_of("KEY").expect("Required");
+            println!("GET: {} -> {}", key, db_store.get(key.to_string())?);
+        }
+        Some(("rm", sub_matches)) => {
+            let key = sub_matches.value_of("KEY").expect("Required");
+            db_store.remove(key.to_string())?;
+            println!("RM: {}", key);
+        }
         _ => unreachable!(),
     }
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,11 @@
 //! `kvs` is a in-memory key value store for storing, retreving and deleting keys
 
 #![deny(missing_docs)]
+mod command;
+use anyhow::{anyhow, Context, Result};
 use std::collections::{hash_map::RandomState, HashMap};
+use std::fs::OpenOptions;
+use std::io::{BufReader, BufWriter};
 
 /// This struct stores the key-value pairs using a HashMap
 pub struct KvStore {
@@ -35,14 +39,50 @@ impl KvStore {
     /// assert_eq!(my_kvs.get(String::from("key1")), Some(String::from("value1")));
     /// assert_eq!(my_kvs.get(String::from("key2")), None);
     /// ```
-    pub fn get(&self, key: String) -> Option<String> {
-        self.hm.get(&key).map(|value| value.to_owned())
+    pub fn get(&mut self, key: String) -> Result<String> {
+        // Open DB
+        let f = OpenOptions::new()
+            .read(true)
+            .open("db.bson")
+            .with_context(|| {
+                "Unable to open the database file: \"db.bson\" for reading".to_string()
+            })?;
+        let mut db_reader = BufReader::new(f);
+        // Restore hash_map
+        loop {
+            match bson::de::from_reader::<_, command::Command>(&mut db_reader) {
+                Ok(doc) => match doc {
+                    command::Command::Set { key, value } => self.hm.insert(key, value),
+                    command::Command::Rm { key } => self.hm.remove(&key),
+                },
+                Err(_) => break,
+            };
+        }
+        let value = self
+            .hm
+            .get(&key)
+            .map(|value| value.to_owned())
+            .ok_or(anyhow!("key: {} not found in store", key))?;
+        Ok(value)
     }
 
     /// Inserts a `key`:`value` pair into the
     /// key value store
-    pub fn set(&mut self, key: String, value: String) {
-        self.hm.insert(key, value);
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        let kvs_command = bson::to_document(&command::Command::Set { key, value })?;
+        // Open DB
+        let f = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open("db.bson")
+            .with_context(|| {
+                "Unable to open the database file: \"db.bson\" for appending".to_string()
+            })?;
+        let mut db_writer = BufWriter::new(f);
+        // Append the command at the end
+        kvs_command.to_writer(&mut db_writer)?;
+        //self.hm.insert(key, value);
+        Ok(())
     }
 
     /// Drops the key value pair from key store
@@ -55,7 +95,19 @@ impl KvStore {
     /// my_kvs.remove( String::from("key1"));
     /// assert_eq!(my_kvs.get(String::from("key1")), None);
     /// ```
-    pub fn remove(&mut self, key: String) {
-        self.hm.remove(&key);
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        let kvs_command = bson::to_document(&command::Command::Rm { key })?;
+        // Open DB
+        let f = OpenOptions::new()
+            .append(true)
+            .open("db.bson")
+            .with_context(|| {
+                "Unable to open the database file: \"db.bson\" for appending".to_string()
+            })?;
+        let mut db_writer = BufWriter::new(f);
+        // Append the command at the end
+        kvs_command.to_writer(&mut db_writer)?;
+        //self.hm.insert(key, value);
+        Ok(())
     }
 }
